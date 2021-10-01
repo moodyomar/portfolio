2e11a6a6450fea4f666e5d02a58d14fe94239da9
moodyomar <lonak.secret@gmail.com>
2021-10-01 10:49:26 +0300

    init project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+GET YOU REACT APP FAST USING DOCKER !!!
+<h2>HEY WE DID IT</h2>
+    </div>
+  );
+}
+
+export default App;
