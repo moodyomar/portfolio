3b9be9f0e3bccfb8aade32aac499a7525d0dab52
moodyomar <lonak.secret@gmail.com>
2021-10-02 16:32:56 +0300

    add dummy data in skills section for mobile testing

--- src/components/cards/SkillCard.js
+++ src/components/cards/SkillCard.js
@@ -2,11 +2,11 @@ import React from 'react';
 import '../styles/SkillCard.css';
 
 
-const SkillCard = ({icon,title,desc}) => { 
+const SkillCard = ({icon,title,desc,direction}) => { 
 
 return(
 
-<div className='SkillCard'>
+<div className='SkillCard' data-aos={`fade-${direction}`}>
 <div className="skillIcon">{icon}</div>
 <div className="skillTitle">{title}</div>
 <div className="skillDesc">{desc}</div>
--- src/components/sections/Skills.js
+++ src/components/sections/Skills.js
@@ -9,9 +9,12 @@ const Skills = () => {
 return(
 
 <div className='Skills'>
+<p data-aos="fade-down" data-aos-duration="2000">--Introduction</p>
+        <h2 data-aos="fade-down" data-aos-duration="2000">Why Hire Me For Next Project?</h2>
+        <p className="para" data-aos="fade-up" data-aos-duration="2200">The technological revolution is changing aspect of our lives, and the fabric of society itself. it's also changing the way we learn and what we learn. Factual knowledge is less prized when everything you ever need to know can be found on your phone. There's no imperative to be an expert at doing everything when you can.</p>
   {skills.map(skill => (
 <SkillCard icon={skill.icon} title={skill.title}
- desc={skill.desc} />
+ desc={skill.desc} direction={skill.animation} />
   ))}
 </div>
 
