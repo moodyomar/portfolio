a32d1e289b5bf4c8d802775ceb799b09cf0def99
moodyomar <lonak.secret@gmail.com>
2021-10-04 19:06:36 +0300

    util bug fix

--- src/utils/customAos.js
+++ src/utils/customAos.js
@@ -1,7 +1,7 @@
 export const customAos = (animation,element) => {
   window.addEventListener('scroll',() => {
     const el = document.querySelector(element);
-    let animationPoin = el.offsetTop - el.offsetHeight;
+    let animationPoin = el.offsetTop - el.offsetHeight - 200;
     if(window.pageYOffset > animationPoin) el.classList.add(animation)
     if(window.pageYOffset < animationPoin) el.classList.remove(animation)
   })
