03a3d4ae22ff23cb42dea197504edc245fb4129b
moodyomar <lonak.secret@gmail.com>
2021-10-06 19:06:06 +0300

    add Testimonials with js styled comp

---
+++ src/components/common/Square.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import '../styles/Testimonials.css'
+
+
+const Square = ({num,text,brT=0,brR=0,brB=0,brL=0,dir}) => { 
+
+return(
+
+<div id="square" style={{
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  width:'300px',
+  flexDirection: 'column',
+  backgroundColor:'#25262a',
+  borderRadius:`${brT}px ${brR}px ${brB}px ${brL}px`
+}} data-aos={`fade-${dir}`} data-aos-duration="1000">
+<h1 style={{color:'#64f4ab'}}>{num}</h1>
+<p>{text}</p>
+</div>
+
+)
+}
+
+
+export default Square
--- src/components/sections/Testimonials.js
+++ src/components/sections/Testimonials.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Square from '../common/Square';
 import '../styles/Testimonials.css'
 
 
@@ -8,9 +9,25 @@ return(
 
 <div className='Testimonials'>
 
+<div id="bar" style={bar}>
+  <Square brT={50} brL={40} num={56} text={'Happy Clients'}
+  dir={'right'} />
+  <Square num={12} text={'Projects Done'} dir={'left'} />
+  <Square num={36} text={'Github Repos'} dir={'right'} />
+  <Square brB={50} brR={40} num={1} text={'Years Experince'}
+  dir={'left'} />
+</div>
+
 </div>
 
 )
+}
+
+const bar = {
+  display:'grid',
+  gridTemplateColumns: 'repeat(4,1fr)',
+  height: '300px',
+  width:'1240px', 
 }
 
 export default Testimonials
