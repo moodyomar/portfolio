c7d8bae6373eac21888b7b326383ce88c5fc93b3
moodyomar <lonak.secret@gmail.com>
2021-10-09 09:52:00 +0300

    responsivity & add contact me section

--- src/components/sections/Testimonials.js
+++ src/components/sections/Testimonials.js
@@ -13,16 +13,22 @@ return(
 <div className='Testimonials'>
 
 <div id="bar" style={bar}>
-  <Square brT={50} brL={40} num={56} text={'Happy Clients'}
+  <Square brT={100} brL={100} num={56} text={'Happy Clients'}
   dir={'right'} />
   <Square num={12} text={'Projects Done'} dir={'left'} />
   <Square num={36} text={'Github Repos'} dir={'right'} />
-  <Square brB={50} brR={40} num={1} text={'Years Experince'}
+  <Square brB={100} brR={100} num={1} text={'Years Experince'}
   dir={'left'} />
 </div>
 
 <div className="qoutes" style={{padding:'0px 100px'}}>
 <Project title={"“Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind.”"} content={"― Bernard M. Baruch"} id={'bernard'} />
+</div>
+
+<div className="contact" style={contactCard}>
+  <h2>Lets Work Together</h2>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis ipsam nihil eaque soluta porro fuga, labore, obcaecati iusto adipisci quam fugiat vero sit repudiandae fugit.</p>
+  <button className="btn">Contact Me</button>
 </div>
 
 </div>
@@ -37,5 +43,16 @@ const bar = {
   width:'1240px', 
   marginBottom:'100px'
 }
-
+const contactCard = {
+  width: '65vw',
+  height: '300px',
+  display: 'flex',
+  flexDirection: 'column',
+  justifContent:' center',
+  alignItems: 'center',
+  margin: '50px 0px',
+  gap:'30px',
+  padding: '40px',
+borderRadius: '50px',
+}
 export default Testimonials
