3dfecc054c5fe3f25ef5626509c82645021df59f
moodyomar <lonak.secret@gmail.com>
2021-10-12 18:13:43 +0300

    add allProjects comp  & map projects in it from json

--- src/components/pages/AllProjects.js
+++ src/components/pages/AllProjects.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import Project from '../common/Project';
+import Splitter from '../common/Splitter';
 
+import projects from '../../data/projects.json'
+import Para from '../common/Para';
 
 const AllProjects = () => { 
 
 return(
 
-<div className=''>
-<h1>AllProjects</h1>
+<div className='Projects'>
+
+<Splitter/>
+
+      {projects.map(project => {
+  let {intro,title,content,btnclass,btn,icon,flip,pId,link,more} = project
+        return(
+          <>
+          <Project intro={intro} title={title} content={content} btnClass={btnclass} btn={btn} icon={icon} id={pId} flip={flip} link={link} more={more} />
+          <Splitter/>
+          </>
+        )
+      })}
 </div>
 
 )
