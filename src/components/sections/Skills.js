044bd195786d7aeb516e713b4e70e6f015a0aed8
moodyomar <lonak.secret@gmail.com>
2021-10-12 16:45:57 +0300

    update cv link

--- src/components/sections/Skills.js
+++ src/components/sections/Skills.js
@@ -18,7 +18,7 @@ const Skills = () => {
           title={'Why Hire Me For Next Project?'}
           para={"Lots of motivation, Consistency, Positive energy and responsibility with great passion. I'm definitely a team player, Very eager to learn new technologies and always strives beyond the sky. Experience working with Wordpress, owner of an active e-commerce store and a small Branding Agency offering business promotions as a side hustle."}
           btn={'Download CV'}
-          link={'https://www.linkedin.com/in/moodyomar/detail/overlay-view/urn:li:fsd_profileTreasuryMedia:(ACoAAC9cRksBw_oW8mDFpsR1eT7819vRIVStggQ,1635466576525)/?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base%3BQHzCPNvQScOhDmpG90WVOQ%3D%3D&licu=urn%3Ali%3Acontrol%3Ad_flagship3_profile_view_base-featured_item_detail_view'} />
+          link={'https://github.com/moodyomar/resume/blob/main/CV-en-Moody.pdf'} />
 
       </div>
       <div className="left-col">
