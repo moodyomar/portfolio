4d1018873a2f12f958d4b3fcf8bf3a6b9e1c667a
moodyomar <lonak.secret@gmail.com>
2021-10-19 12:21:55 +0300

    update cv

--- src/components/sections/Skills.js
+++ src/components/sections/Skills.js
@@ -5,7 +5,7 @@ import SkillCard from '../cards/SkillCard';
 import skills from '../../data/skills.json';
 import Para from '../common/Para';
 
-import cv from '../../assets/CV-en-Moody.pdf'
+import cv from '../../assets/cv-en-moody.pdf'
 import '../styles/Skills.css';
 
 
